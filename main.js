77f767fda00067017c995689406b5a00199e8336
farwa-faraz <99012312+farwa-faraz@users.noreply.github.com>
2023-01-24 17:10:38 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -20,17 +20,87 @@ function snapshot()
 
 console.log("ml5 version is:", ml5.version);
 
-classify = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/3DctCFBnA/model.json", modelLoaded);
+Classify = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/3DctCFBnA/model.json", modelLoaded);
 
 function modelLoaded() 
 {
     console.log("The model has been loaded");
 }
 
-function speak()
+function gesture()
 {
-    var Speak = window.speechSynthesis;
-    speak_1 = "The first prediction is" + prediction_1;
-    var text_to_speak = new SpeechSynthesisUtterance(speak_1);
-    Speak.speak(text_to_speak);
+    image = document.getElementById("image");
+    Classify.classify(image, gotResults);
 }
+
+function gotResults(error, results)
+{
+    if (error)
+    {
+        console.error(error);
+    }
+    else
+    {
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        prediction_1 = results[0].label;
+        
+        if(prediction_1 == "Cool")
+        {
+            document.getElementById("emoji").innerHTML = "&#129304;";
+            var Speak = window.speechSynthesis;
+            speak_1 = "Thats so cool";
+            var text_to_speak = new SpeechSynthesisUtterance(speak_1);
+            Speak.speak(text_to_speak);
+        }
+
+        if(prediction_1 == "Amazing")
+        {
+            document.getElementById("emoji").innerHTML = "&#128076;";
+            var Speak = window.speechSynthesis;
+            speak_1 = "Thats Amazing";
+            var text_to_speak = new SpeechSynthesisUtterance(speak_1);
+            Speak.speak(text_to_speak);
+        }
+
+        if(prediction_1 == "Victory")
+        {
+            document.getElementById("emoji").innerHTML = "&#9996;";
+            var Speak = window.speechSynthesis;
+            speak_1 = "That was a marvelous victory";
+            var text_to_speak = new SpeechSynthesisUtterance(speak_1);
+            Speak.speak(text_to_speak);
+        }
+
+        if(prediction_1 == "Excellent")
+        {
+            document.getElementById("emoji").innerHTML = "&#128077;";
+            document.getElementById("emoji").innerHTML = "&#9996;";
+            var Speak = window.speechSynthesis;
+            speak_1 = "That was excellent";
+            var text_to_speak = new SpeechSynthesisUtterance(speak_1);
+            Speak.speak(text_to_speak);
+        }
+
+        if(prediction_1 == "Angry")
+        {
+            document.getElementById("emoji").innerHTML = "&#9994;";
+            document.getElementById("emoji").innerHTML = "&#9996;";
+            var Speak = window.speechSynthesis;
+            speak_1 = "My mum looks so angry";
+            var text_to_speak = new SpeechSynthesisUtterance(speak_1);
+            Speak.speak(text_to_speak);
+        }
+
+        if(prediction_1 == "Clapping")
+        {
+            document.getElementById("emoji").innerHTML = "&#128079;";
+            document.getElementById("emoji").innerHTML = "&#9996;";
+            var Speak = window.speechSynthesis;
+            speak_1 = "Be happy, everyone is clapping for you";
+            var text_to_speak = new SpeechSynthesisUtterance(speak_1);
+            Speak.speak(text_to_speak);
+        }
+    }
+}
+
